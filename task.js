0fbb956890feacb2e4806c010a2f4dad00a4f8d6
Srijanibanerjee <90030341+Srijanibanerjee@users.noreply.github.com>
2021-11-28 18:45:12 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,4 @@
+mydata = `[{"id" : 1, "task" : "upload your Addhar card"},
+{"id" : 2, "task" : "upload your resume"},
+{"id" : 3, "task" : "upload your pan card"},
+{"id" : 4, "task" : "upload your result card"}]`;
